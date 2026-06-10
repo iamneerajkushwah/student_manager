8c117cd4e0165dadb0a04e2423ae3ea51ff881ba
iamneerajkushwah <iamneeraj10010@gmail.com>
2026-06-10 23:18:33 +0530

    JWT Authentication

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
-from routes.student import router
+from routes.student import router as student_router
+from routes.auth import router as auth_router
 
 app = FastAPI()
 
-app.include_router(router)
+app.include_router(student_router)
+app.include_router(auth_router)
+
--- routes/auth.py
+++ routes/auth.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException
 
 from database import users_collection
-from models.user import User
-from utils.security import hash_password
+from models.user import User, UserLogin
+from utils.security import hash_password, verify_password
+from utils.token import create_acess_token
 
 router = APIRouter(
     tags=["Athuentication"]
@@ -11,6 +12,61 @@ router = APIRouter(
 @router.post("/register")
 def register(user: User):
 
-    existing_user = users_collection()
+    existing_user = users_collection.find_one(
+        {"username": user.username}
+    )
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Username already exists"
+        )
+    
+    hashed_password = hash_password(
+        user.password
+    )
+
+    users_collection.insert_one(
+        {
+            "username": user.username,
+            "password": hashed_password
+        }
+    )
+
+    return {
+        "message": "User regitered successfully"
+    }
+
+
+@router.post("/login")
+def login(user: UserLogin):
+
+    db_user = users_collection.find_one(
+        {"username":user.username}
+    )
+
+    if not db_user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password"
+        )
+    
+    if not verify_password(
+        user.password,
+        db_user["password"]
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid username or password"
+        )
+    
+    access_token = create_acess_token(
+        {"sub": user.username}
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
 
 
